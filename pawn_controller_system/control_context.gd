a1e23245d09a63f192b47b80a645f70db14bc969
PolanskiPol <polanski.real@gmail.com>
2024-08-09 19:22:29 +0200

    chore: Move pawn_controller_system to its own repo (equinox)

--- pawn_controller_system/control_context.gd
+++
@@ -1,4 +0,0 @@
-class_name ControlContext
-## Base class for all Contexts related to controls
-## Extend this class to create a new context, and add @export variables to communicate between a [Controller] and [Pawn]
-extends Resource
