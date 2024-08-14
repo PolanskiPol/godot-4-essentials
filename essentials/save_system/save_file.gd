21e9287f9d3367bd1c162604f0412a079ee268c3
PolanskiPol <polanski.real@gmail.com>
2024-08-14 18:19:21 +0200

    fix: Fix Settings and change SaveMeta
    * New Class: SaveMetas, holds a dictionary of Class SaveMeta
    * Class SaveMeta now holds data as @export properties, can be extended
    * load_settings and save_settings now work correctly
    * Method get_meta_info removed forever from Class SettingsFile

--- essentials/save_system/save_file.gd
+++ essentials/save_system/save_file.gd
@@ -1,14 +1,3 @@
 @icon("isave_file.svg")
 class_name SaveFile
 extends Resource
-
-## Virtual function. Defines the save's metadata to be read (for example) on a 'Select file to load' screen.
-## Overwrite to set custom metadata.
-func get_meta_info() -> Dictionary:
-	return {
-		"name": "meta_name",
-		"description": "meta_description",
-		"create_date": "0000-00-00 00:00",
-		"modify_date": "0000-00-00 00:00",
-		"version": 1
-	}
--- essentials/save_system/settings_file.gd
+++ essentials/save_system/settings_file.gd
@@ -1,14 +1,3 @@
 @icon("isettings_file.svg")
 class_name SettingsFile
 extends Resource
-
-## Virtual function. Defines the save's metadata to be read (for example) on a 'Select file to load' screen.
-## Overwrite to set custom metadata.
-## This function may never be used in the future.
-## @experimental
-func get_meta_info() -> Dictionary:
-	return {
-		"name": "meta_name",
-		"modify_date": "0000-00-00 00:00",
-		"version": 1
-	}
